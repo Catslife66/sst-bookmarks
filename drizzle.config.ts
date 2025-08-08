babbb9d717f929af22e1251a05f50aa365822305
Catslife66 <josie58@live.cn>
2025-08-08 12:05:23 +0100

    Updated drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,11 @@
-import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
-
-config({ path: ".env" });
+import { Resource } from "sst";
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./app/lib/schema.js",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: Resource.DATABASE_URL.value!,
   },
 });
