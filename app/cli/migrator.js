f4bdec37393629f5ab9cb16dbb00a5ba596148de
Catslife66 <josie58@live.cn>
2025-08-08 12:16:45 +0100

    Updated migrate workflow 2

--- app/cli/migrator.js
+++ app/cli/migrator.js
@@ -1,3 +1,8 @@
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import { drizzle } from "drizzle-orm/neon-serverless";
 import { Pool, neonConfig } from "@neondatabase/serverless";
+import { schema } from "../lib/schema";
+
+import ws from "ws";
+
+async function performMigrate() {}
